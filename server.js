8e5e58ef7d8a7906c6e0749d09a998082847b0d7
Navet21 <pablosantanaftv@hotmail.com>
2024-12-30 11:56:37 +0000

    Api para render

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express') 
+const logger = require('morgan')
+const errorhandler = require('errorhandler')
+const bodyParser = require('body-parser')
+
+//Store
+
+let store = {}
+store.accounts = []
+
+//Express
+
+let app = express()
+
+// Uso de middlewares
+app.use(bodyParser.json()) // Middleware para parsear JSON
+app.use(logger('dev')) // Middleware para registrar solicitudes en la consola
+app.use(errorhandler()) // Middleware para manejar errores en modo desarrollo
+
+
+//Endpoints
+
+//Recibir las cuentas.
+app.get('/accounts', (req, res) => {
+    res.status(200).send(store.accounts)
+})
+
+//Registrar una cuenta
+app.post('/accounts', (req, res) => {
+    let newAccount = req.body
+    let id = store.accounts.length
+    store.accounts.push(newAccount)
+    res.status(201).send({id: id})
+})
+
+//Modificar las cuentas
+app.put('/accounts/:id', (req, res) => {
+    store.accounts[req.params.id] = req.body
+    res.status(200).send(store.accounts[req.params.id])
+})
+
+//Eliminar la cuenta
+app.delete('/accounts/:id', (req, res) => {
+    store.accounts.splice(req.params.id, 1)
+    res.status(204).send()
+})
+
+//Consultar un unico registro
+app.get('/accounts/:id',(req,res) =>{
+    let id = req.params.id
+    res.status(200).send(store.accounts[id])
+})
+
+//Puerto
+app.listen(3000)
